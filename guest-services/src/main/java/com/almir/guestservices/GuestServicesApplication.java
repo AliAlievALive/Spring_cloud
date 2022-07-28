7b23a025914bd10ff824681d7f81540dd9d7fa81
almir <madarat87@gmail.com>
2022-07-28 07:39:31 +0300

    Added my services to Eureka. Deleted lombok runtime

--- guest-services/src/main/java/com/almir/guestservices/GuestServicesApplication.java
+++ guest-services/src/main/java/com/almir/guestservices/GuestServicesApplication.java
@@ -2,10 +2,11 @@ package com.almir.guestservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GuestServicesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GuestServicesApplication.class, args);
 	}
